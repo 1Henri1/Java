a996d37f90b849b0f775ff62ad6d935053521ad1
1Henri1 <125100866+1Henri1@users.noreply.github.com>
2023-04-04 21:33:35 -0300

    exercise of imc

--- 04_04_23/src/main/Main.java
+++ 04_04_23/src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        
+        /*
         String x = JOptionPane.showInputDialog(null,"digite seu nome");
         String y = JOptionPane.showInputDialog(null,"digite a nota do primeiro bimestre");
         double nota1 = Double.parseDouble(y);
@@ -20,7 +20,43 @@ public class Main {
        
         
         JOptionPane.showMessageDialog(null, "o aluno "+ x + "\n teve como média: " + String.format("%.2f",b) );
+    */  
+        
+        
+        /*
+         String x = JOptionPane.showInputDialog(null,"digite seu peso");
+          double peso = Double.parseDouble(x);
+         String y = JOptionPane.showInputDialog(null,"digite sua altura");
+          double altura = Double.parseDouble(y);
+          
+         double z = peso / (altura * altura);
+         
+         if (z<18.5){
+             JOptionPane.showMessageDialog(null, "Você esta abaixo do peso");
+    }else if(z<24.9){
+            JOptionPane.showMessageDialog(null, "Você esta no peso normal");
+    }else if(z<29.9){
+            JOptionPane.showMessageDialog(null, "Você esta em sobrepeso");
+    }else if(z<34.9){
+            JOptionPane.showMessageDialog(null, "Você esta com obesidade leve");
+    }else if(z<39.9){
+            JOptionPane.showMessageDialog(null, "Você esta com obesidade moderada");
+    }else{
+            JOptionPane.showMessageDialog(null, "Você esta com obesidade mórbida");
     }
+*/
+         
+         Object[] op = {"1","2","3","4"};
+         
+         JOptionPane.showInputDialog( null , "Escolha" , "Titulo" ,
+        JOptionPane.QUESTION_MESSAGE , null , op , "tres" );
+
+    }
+    }
+        
+
+        
+    
     
         
-}
+
